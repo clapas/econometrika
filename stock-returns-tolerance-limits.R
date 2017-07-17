4c5978f0bca46e5de64159159917bd78d0dcc789
Claudio Pascual <claudio.pascual@gmail.com>
2017-07-17 18:11:59 +0100

    uwsgi necesita el __init__; faltaba library

--- stock-returns-tolerance-limits.R
+++ stock-returns-tolerance-limits.R
@@ -20,6 +20,7 @@ dbClearResult(stmt)
 stmt <- postgresqlExecStatement(con, 'insert into analysis_keyvalue (category, key, value) values ($1, $2, $3), ($4, $5, $6)',
     c(category, 'ibex35_max_gain_date', as.character(ibex35$date[match(max(ibex35$rate), ibex35$rate)]), category, 'ibex35_max_gain', prettyNum(exp(max(ibex35$rate))*100 - 100)))
 dbClearResult(stmt)
+library(tolerance)
 stmt <- postgresqlExecStatement(con, 'insert into analysis_keyvalue (category, key, value) values ($1, $2, $3), ($4, $5, $6)',
     c(category, 'ibex35_0.999_bitol_D', prettyNum(distfree.est(libex35, 0.001, side=2)), category, 'ibex35_0.99_bitol_D', prettyNum(distfree.est(libex35, 0.01, side=2))))
 dbClearResult(stmt)
