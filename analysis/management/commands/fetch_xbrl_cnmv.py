03159d0b999370cf525477088f04871959ed4dc1
Claudio Pascual <claudio.pascual@gmail.com>
2017-09-14 14:26:27 +0100

    Error espúreo, posib. combinacion vim pasó a mayúsc.

--- analysis/management/commands/fetch_xbrl_cnmv.py
+++ analysis/management/commands/fetch_xbrl_cnmv.py
@@ -20,7 +20,7 @@ QUARTER_TOKEN = 'trimestre'
 class WritableDir(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         prospective_dir=values
-        if not OS.path.isdir(prospective_dir):
+        if not os.path.isdir(prospective_dir):
             raise argparse.ArgumentError(self, "{0} is not a valid path".format(prospective_dir))
         if os.access(prospective_dir, os.W_OK):
             setattr(namespace,self.dest,prospective_dir)
